f8b1effbcba0ff31cd7664fb37fda6530eeafcfc
NathanAyres <nathan.ayres16@gmail.com>
2020-12-15 23:14:12 -0300

    project init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+import Conversor from "./components/Conversor"
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Conversor de Moedas</h1>
+        <div className="linha">
+          <Conversor moedaA ="USD" moedaB ="BRL"></Conversor>
+          <Conversor moedaA ="BRL" moedaB ="USD"></Conversor>
+        </div>
+        <div className="linha">
+          <Conversor moedaA ="EUR" moedaB ="BRL"></Conversor>
+          <Conversor moedaA ="BRL" moedaB ="EUR"></Conversor>
+        </div>
+        <div className="linha">
+          <Conversor moedaA ="AUD" moedaB ="BRL"></Conversor>
+          <Conversor moedaA ="BRL" moedaB ="AUD"></Conversor>
+        </div>
+      </div>
+    );
+   
+}
+
+export default App;
